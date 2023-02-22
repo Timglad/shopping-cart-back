ac90af93ea5e9d27deaa645b25aa09ad1952e9a0
Tim_Glad <timothyboi64@gmail.com>
2023-02-23 00:03:29 +0200

    edit product urls

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.products),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
+    path('product/<int:pk>/', views.product_detail),
     path('cart/', views.cart),
+    path('cart/<int:pk>/', views.cart),
 ]
